989aa630f78b2fbb701baa2a6f83ee1c871105bf
vatsal <vso.opash@gmail.com>
2023-12-27 18:06:21 +0530

    change env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,10 @@
 export const environment = {
-  production: false,
+  production: true,
   hmr: false,
-  serverUrl: 'https://dev-api.freedom.buzz/api/v1/',
+  serverUrl: 'https://api.insuranceagent.tube/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://dev-api.freedom.buzz/',
+  socketUrl: 'https://api.insuranceagent.tube/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,10 @@
 export const environment = {
-  production: false,
+  production: true,
   hmr: false,
-  serverUrl: 'https://freedom-api.opash.in/api/v1/',
+  serverUrl: 'https://api.insuranceagent.tube/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://freedom-api.opash.in/',
+  socketUrl: 'https://api.insuranceagent.tube/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
+
 };
