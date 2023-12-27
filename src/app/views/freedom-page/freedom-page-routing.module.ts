7b7f29138436b5cdce21911820062feda3ce9e81
vatsal <vso.opash@gmail.com>
2023-12-27 17:52:48 +0530

    Change logo and wording

--- src/app/views/freedom-page/freedom-page-routing.module.ts
+++ src/app/views/freedom-page/freedom-page-routing.module.ts
@@ -8,14 +8,14 @@ const routes: Routes = [
     path: '',
     component: CommunityComponent,
     data: {
-      title: 'Healing Topics',
+      title: 'Insurance Products',
     },
   },
   {
     path: 'edit/:id',
     component: EditCommunityComponent,
     data: {
-      title: 'Edit Topic',
+      title: 'Edit Product',
     },
   },
 ];
